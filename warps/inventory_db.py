397830814e4c71c8c1e2c085b4c85cd2841084d5
Nauf <130215373+naufudge@users.noreply.github.com>
2023-06-27 21:48:52 +0500

    Delete inventory_db.py
    contains sensitive information

--- warps/inventory_db.py
+++
@@ -1,6 +0,0 @@
-from urllib.parse import quote_plus
-
-user = quote_plus('nauf')
-password = quote_plus('Celesti@l143')
-
-uri = f"mongodb+srv://{user}:{password}@pompomcluster.rk76yym.mongodb.net/?retryWrites=true&w=majority"
