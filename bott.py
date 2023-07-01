783e0a25a8c6ec3c44fc64f2b97e2ceceb14cd9f
naufudge <130215373+naufudge@users.noreply.github.com>
2023-07-01 19:11:49 +0500

    changed to sharded bot along with error handling

--- bott.py
+++ bott.py
@@ -1,32 +1,39 @@
 import discord
 import os
 from discord.ext import commands
+from discord.ext.commands import errors
 
-# I changed your bot class to AutoShardedBot. You will need to shard soon if not immediately
 class PomPomClient(commands.AutoShardedBot):
     def __init__(self, **kwargs):
         # REMEMBER TO CHANGE THE INTENTS BEFORE FINALING
         intents = discord.Intents.default()
         # intents.message_content = True
-        super().__init__(**kwargs, command_prefix="pom$", intents=intents, help_command=None)
-
-        # You are able to set the status here just by setting self.activity
+        super().__init__(**kwargs, command_prefix=commands.when_mentioned_or('pom$'), intents=intents, help_command=None)
         self.activity = discord.Game(name="/warp | Star Rail ✦")
 
     # Because I seperated your cogs into a cog folder, we can just iterate over the folder instead of having to hard-code cog names with self.poms
     async def setup_hook(self):
-        for filename in os.listdir(f'{os.getcwd()}/cogs'):
+        for filename in [file for file in os.listdir(f'{os.getcwd()}/cogs') if file.lower().endswith('.py')]:
             await self.load_extension(f'cogs.{filename[:-3]}')
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx: commands.Context, error):
+        if isinstance(error, errors.MissingPermissions):
+            await ctx.send(f":x: You can't use that command.", ephemeral=True)
+        elif isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send(f":x: Required arguments aren't passed.", ephemeral=True)
+        elif isinstance(error, errors.CommandNotFound):
+            pass
+        elif isinstance(error, errors.NotOwner):
+            pass
+        else:
+            # report_channel = self.get_channel(id=1123923664941883422)
+            # embed = discord.Embed(title='An Error has occurred', description=f'Error: \n `{error}`', timestamp=ctx.message.created_at, color=0x242424)
+            # await report_channel.send(embed=embed)
+            print(error)
 
     async def on_ready(self):
         # You can use a logger here instead of printing: https://discordpy.readthedocs.io/en/stable/logging.html
         # The logger can be used globally through self.client.log.info("some string here")
         print(f'{self.user.name} has connected to Discord!, and now on {len(self.guilds)} servers!')
-        try:
-            # If you go to cluster, you will need to avoid syncing your commands this way. It will send too many requests to Discord
-            # The recommended way is manually with a command (inconvenient, but if you update command arguments at all you can just
-            # sync the tree instead of restarting the entire bot)
-            synced = await self.tree.sync()
-            print(f"Synced {len(synced)} command(s)")
-        except Exception as e:
-            print(e)
+
