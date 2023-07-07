228560e8491feae7485d7fe75fcb017f71dd5693
naufudge <130215373+naufudge@users.noreply.github.com>
2023-07-07 23:43:56 +0500

    Error handling updates & DB inherited to client

--- bott.py
+++ bott.py
@@ -1,22 +1,26 @@
 import discord
-import os
+from discord import app_commands
 from discord.ext import commands, tasks
 from discord.ext.commands import errors
-from concurrent.futures import ThreadPoolExecutor
-import topgg
+# from concurrent.futures import ThreadPoolExecutor
+# import topgg
+import os
 
 class PomPomClient(commands.AutoShardedBot):
     def __init__(self, **kwargs):
-        # REMEMBER TO CHANGE THE INTENTS BEFORE FINALING
         intents = discord.Intents.default()
-        # intents.message_content = True
         super().__init__(**kwargs, command_prefix=commands.when_mentioned_or('pom$'), intents=intents, help_command=None)
         self.activity = discord.Game(name="/warp | Star Rail ✦")
         # self.topggpy = topgg.DBLClient(self, kwargs['topgg_token'])
         # self.executor = ThreadPoolExecutor()
 
-    # Because I seperated your cogs into a cog folder, we can just iterate over the folder instead of having to hard-code cog names with self.poms
     async def setup_hook(self):
+        try:
+            # REMEMBER TO CHANGE THIS WHEN TESTING
+            self.user_data_collection = self.mongoConnect['PomPomDB']['tester']
+        except AttributeError:
+            print("Not Connected to PomPomDB :(")
+
         for filename in [file for file in os.listdir(f'{os.getcwd()}/cogs') if file.lower().endswith('.py')]:
             await self.load_extension(f'cogs.{filename[:-3]}')
 
@@ -30,11 +34,12 @@ class PomPomClient(commands.AutoShardedBot):
             pass
         elif isinstance(error, errors.NotOwner):
             pass
+        elif isinstance(error, errors.HybridCommandError) and isinstance(error.original, app_commands.CommandOnCooldown):
+            pass
+        # elif isinstance(error, errors.HybridCommandError) and isinstance(error.original, app_commands.CommandInvokeError):
+        #     pass
         else:
-            # report_channel = self.get_channel(id=1123923664941883422)
-            # embed = discord.Embed(title='An Error has occurred', description=f'Error: \n `{error}`', timestamp=ctx.message.created_at, color=0x242424)
-            # await report_channel.send(embed=embed)
-            print(error)
+            print(f"{error.__class__.__name__}: {error}")
 
     # @tasks.loop(hours=12)
     # async def update_stats(self):
@@ -47,6 +52,5 @@ class PomPomClient(commands.AutoShardedBot):
     async def on_ready(self):
         # You can use a logger here instead of printing: https://discordpy.readthedocs.io/en/stable/logging.html
         # The logger can be used globally through self.client.log.info("some string here")
-        print(f'{self.user.name} has connected to Discord!, and now on {len(self.guilds)} servers!')
-        # self.update_stats.start()
+        print(f'{self.user.name} has connected to Discord!, and now on {len(self.guilds)} servers! \nShard count: {self.shard_count}')
 
