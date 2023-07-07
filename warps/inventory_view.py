9412bb19e9722d82d07f9cf8a5fb4265ad112d8b
naufudge <130215373+naufudge@users.noreply.github.com>
2023-07-07 23:48:52 +0500

    Removed redundant code

--- warps/inventory_view.py
+++ warps/inventory_view.py
@@ -1,11 +1,11 @@
 from PIL import Image, ImageFont, ImageDraw, ImageOps
 import math
-import json
+from helpers.pom_funcs import all_warp_details
 
 
 def human_format(num: int):
     """
-    Changes long numbers to short form. Eg: 10,000 -> 10K
+    Changes long numbers to short form. Eg: 10000 -> 10K
     """
     num = float('{:.3g}'.format(num))
     magnitude = 0
@@ -15,11 +15,8 @@ def human_format(num: int):
     return '{}{}'.format('{:f}'.format(num).rstrip('0').rstrip('.'), ['', 'K', 'M', 'B', 'T'][magnitude])
 
 def charas_by_rarity_sort(initial_dict: dict):
-    with open('warps/data/all_warps.json', 'r') as all_warps_data_file:
-        chara_and_wpn_data = json.load(all_warps_data_file)
-
     list_of_owned_charas = list(initial_dict)
-    list_of_owned_charas_with_rarity = sorted([(chara_and_wpn_data[chara]['rarity'], chara) for chara in list_of_owned_charas], reverse=True)
+    list_of_owned_charas_with_rarity = sorted([(all_warp_details[chara]['rarity'], chara) for chara in list_of_owned_charas], reverse=True)
 
     new_dict_of_owned_charas, chara_rarity = {}, {}
 
@@ -28,7 +25,6 @@ def charas_by_rarity_sort(initial_dict: dict):
         chara_rarity[chara] = rarity
 
     return (new_dict_of_owned_charas, chara_rarity)
-
 
 def inventory_view(inventory_dict: dict):
     if inventory_dict == {}:
